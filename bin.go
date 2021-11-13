2323e7ada6eb2ce7a6b8049185d96909de45fe9d
Kevin C. Krinke <kevin@krinke.ca>
2021-11-13 11:37:16 -0500

    Bin: refactored doc strings; removed ShowAll as Container.ShowAll is identical; added MakeBin and NewBin constructors; code cosmetic changes

--- bin.go
+++ bin.go
@@ -4,7 +4,6 @@ import (
 	"github.com/go-curses/cdk"
 )
 
-// CDK type-tag for Bin objects
 const TypeBin cdk.CTypeTag = "ctk-bin"
 
 func init() {
@@ -28,11 +27,11 @@ func init() {
 //	        +- ToolItem
 //	        +- ScrolledWindow
 //	        +- Viewport
-// The Bin widget is a container with just one child. It is not very useful
+//
+// The Bin Widget is a Container with just one child. It is not very useful
 // itself, but it is useful for deriving subclasses, since it provides common
 // code needed for handling a single child widget. Many CTK widgets are
-// subclasses of Bin, including Window, Button, Frame, HandleBox or
-// ScrolledWindow.
+// subclasses of Bin, including Window, Button, Frame or ScrolledWindow.
 type Bin interface {
 	Container
 	Buildable
@@ -40,21 +39,34 @@ type Bin interface {
 	Init() (already bool)
 	GetChild() (value Widget)
 	Add(w Widget)
-	ShowAll()
 }
 
-// The CBin structure implements the Bin interface and is
-// exported to facilitate type embedding with custom implementations. No member
-// variables are exported as the interface methods are the only intended means
-// of interacting with Bin objects
+// The CBin structure implements the Bin interface and is exported to
+// facilitate type embedding with custom implementations. No member variables
+// are exported as the interface methods are the only intended means of
+// interacting with Bin objects.
 type CBin struct {
 	CContainer
 }
 
-// Bin object initialization. This must be called at least once to setup
-// the necessary defaults and allocate any memory structures. Calling this more
-// than once is safe though unnecessary. Only the first call will result in any
-// effect upon the Bin instance
+// MakeBin is used by the Buildable system to construct a new Bin.
+func MakeBin() *CBin {
+	return NewBin()
+}
+
+// NewBin is the constructor for new Bin instances.
+func NewBin() *CBin {
+	a := new(CBin)
+	a.Init()
+	return a
+}
+
+// Init initializes a Bin object. This must be called at least once to
+// set up the necessary defaults and allocate any memory structures. Calling
+// this more than once is safe though unnecessary. Only the first call will
+// result in any effect upon the Bin instance. Init is used in the
+// NewBin constructor and only necessary when implementing a derivative
+// Bin type.
 func (b *CBin) Init() (already bool) {
 	if b.InitTypeItem(TypeBin, b) {
 		return true
@@ -65,12 +77,8 @@ func (b *CBin) Init() (already bool) {
 	return false
 }
 
-// Gets the child of the Bin, or NULL if the bin contains no child widget.
-// The returned widget does not have a reference added, so you do not need to
-// unref it.
-// Returns:
-// 	pointer to child of the Bin.
-// 	[transfer none]
+// GetChild is a convenience method to return the first child in the Bin
+// Container. Returns the Widget or `nil` if the Bin contains no child widget.
 func (b *CBin) GetChild() (value Widget) {
 	if len(b.children) > 0 {
 		value = b.children[0]
@@ -88,11 +96,4 @@ func (b *CBin) Add(w Widget) {
 		}
 	}
 	b.CContainer.Add(w)
-}
-
-func (b *CBin) ShowAll() {
-	b.Show()
-	for _, child := range b.children {
-		child.ShowAll()
-	}
 }
