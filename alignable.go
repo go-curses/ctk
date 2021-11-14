f716ccfaacbc9a63aca2fd86bf60ce3e52ee59bb
Kevin C. Krinke <kevin@krinke.ca>
2021-11-13 22:07:45 -0500

    Alignable: added doc string

--- alignable.go
+++ alignable.go
@@ -1,5 +1,8 @@
 package ctk
 
+// An Alignable Widget is one that implements the SetAlignment and GetAlignment
+// methods for adjusting the positioning of the Widget. The Misc and Alignment
+// types are the primary ones implementing this interface.
 type Alignable interface {
 	SetAlignment(xAlign float64, yAlign float64)
 	GetAlignment() (xAlign float64, yAlign float64)
