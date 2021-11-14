0c2240fc2e6f7a401d23b0c9382412b145b5dcea
Kevin C. Krinke <kevin@krinke.ca>
2021-11-14 16:03:43 -0500

    Misc: refactored doc strings; code cosmetic changes

--- misc.go
+++ misc.go
@@ -5,7 +5,6 @@ import (
 	cmath "github.com/go-curses/cdk/lib/math"
 )
 
-// CDK type-tag for Misc objects
 const TypeMisc cdk.CTypeTag = "ctk-misc"
 
 func init() {
@@ -20,29 +19,35 @@ func init() {
 //            +- Arrow
 //            +- Image
 //            +- Pixmap
+//
+// The Misc Widget is intended primarily as a base type for other Widget
+// implementations where there is a necessity for alignment and padding
+// properties.
 type Misc interface {
 	Widget
 	Buildable
 
 	Init() (already bool)
-	SetAlignment(xAlign float64, yAlign float64)
-	SetPadding(xPad int, yPad int)
 	GetAlignment() (xAlign float64, yAlign float64)
+	SetAlignment(xAlign float64, yAlign float64)
 	GetPadding() (xPad int, yPad int)
+	SetPadding(xPad int, yPad int)
 }
 
-// The CMisc structure implements the Misc interface and is
-// exported to facilitate type embedding with custom implementations. No member
-// variables are exported as the interface methods are the only intended means
-// of interacting with Misc objects
+// The CMisc structure implements the Misc interface and is exported
+// to facilitate type embedding with custom implementations. No member variables
+// are exported as the interface methods are the only intended means of
+// interacting with Misc objects.
 type CMisc struct {
 	CWidget
 }
 
-// Misc object initialization. This must be called at least once to setup
-// the necessary defaults and allocate any memory structures. Calling this more
-// than once is safe though unnecessary. Only the first call will result in any
-// effect upon the Misc instance
+// Init initializes a Misc object. This must be called at least once to
+// set up the necessary defaults and allocate any memory structures. Calling
+// this more than once is safe though unnecessary. Only the first call will
+// result in any effect upon the Misc instance. Init is used in the
+// NewMisc constructor and only necessary when implementing a derivative
+// Misc type.
 func (m *CMisc) Init() (already bool) {
 	if m.InitTypeItem(TypeMisc, m) {
 		return true
@@ -55,6 +60,17 @@ func (m *CMisc) Init() (already bool) {
 	return false
 }
 
+// GetAlignment returns the X and Y alignment of the widget within its
+// allocation.
+// See: SetAlignment()
+func (m *CMisc) GetAlignment() (xAlign float64, yAlign float64) {
+	xAlign, _ = m.GetFloatProperty(PropertyXAlign)
+	yAlign, _ = m.GetFloatProperty(PropertyYAlign)
+	return
+}
+
+// SetAlignment is a convenience method to set the x-align and y-align
+// properties of the Misc Widget.
 func (m *CMisc) SetAlignment(xAlign float64, yAlign float64) {
 	xAlign = cmath.ClampF(xAlign, 0.0, 1.0)
 	yAlign = cmath.ClampF(yAlign, 0.0, 1.0)
@@ -66,6 +82,16 @@ func (m *CMisc) SetAlignment(xAlign float64, yAlign float64) {
 	}
 }
 
+// GetPadding returns the padding in the X and Y directions of the widget.
+// See: SetPadding()
+func (m *CMisc) GetPadding() (xPad int, yPad int) {
+	xPad, _ = m.GetIntProperty(PropertyXPad)
+	yPad, _ = m.GetIntProperty(PropertyYPad)
+	return
+}
+
+// SetPadding is a convenience method to set x-pad and y-pad properties of the
+// Misc Widget.
 func (m *CMisc) SetPadding(xPad int, yPad int) {
 	if err := m.SetIntProperty(PropertyXPad, xPad); err != nil {
 		m.LogErr(err)
@@ -73,32 +99,6 @@ func (m *CMisc) SetPadding(xPad int, yPad int) {
 	if err := m.SetIntProperty(PropertyYPad, yPad); err != nil {
 		m.LogErr(err)
 	}
-}
-
-// Gets the X and Y alignment of the widget within its allocation. See
-// SetAlignment.
-// Parameters:
-//      xAlign  location to store X alignment of misc
-// , or NULL.
-//      yAlign  location to store Y alignment of misc
-// , or NULL.
-func (m *CMisc) GetAlignment() (xAlign float64, yAlign float64) {
-	xAlign, _ = m.GetFloatProperty(PropertyXAlign)
-	yAlign, _ = m.GetFloatProperty(PropertyYAlign)
-	return
-}
-
-// Gets the padding in the X and Y directions of the widget. See
-// SetPadding.
-// Parameters:
-//      xPad    location to store padding in the X
-// direction, or NULL.
-//      yPad    location to store padding in the Y
-// direction, or NULL.
-func (m *CMisc) GetPadding() (xPad int, yPad int) {
-	xPad, _ = m.GetIntProperty(PropertyXPad)
-	yPad, _ = m.GetIntProperty(PropertyYPad)
-	return
 }
 
 // The horizontal alignment, from 0 (left) to 1 (right). Reversed for RTL
