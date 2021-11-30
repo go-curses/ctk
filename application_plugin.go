f0f008f2114ee3e3285846f9d2f9b658f6d848cb
Kevin C. Krinke <kevin@krinke.ca>
2021-11-30 12:23:05 -0500

    application_plugin.go: document NewApplicationFromPlugin; renamed func declarations to remove the Plugin part (these are useful beyond application plugins); implemented CTK versions of ArgvApplicationSignalStartup and the various WithArgv* signal listener helpers; code cosmetic changes

--- application_plugin.go
+++ application_plugin.go
@@ -10,63 +10,51 @@ import (
 	cenums "github.com/go-curses/cdk/lib/enums"
 )
 
-func ArgvApplicationSignalStartup(argv ...interface{}) (app Application, display cdk.Display, ctx context.Context, cancel context.CancelFunc, wg *sync.WaitGroup, ok bool) {
-	if len(argv) == 5 {
-		if app, ok = argv[0].(Application); ok {
-			if display, ok = argv[1].(cdk.Display); ok {
-				if ctx, ok = argv[2].(context.Context); ok {
-					if cancel, ok = argv[3].(context.CancelFunc); ok {
-						if wg, ok = argv[4].(*sync.WaitGroup); ok {
-							return
-						}
-						cancel = nil
-					}
-					ctx = nil
-				}
-				display = nil
-			}
-			app = nil
-		}
-	}
-	return
-}
-
-func WrapArgvApplicationSignalStartup(startupFn ApplicationPluginStartupFn) cdk.SignalListenerFn {
-	return func(_ []interface{}, argv ...interface{}) cenums.EventFlag {
-		if app, display, ctx, cancel, wg, ok := ArgvApplicationSignalStartup(argv...); ok {
-			return startupFn(app, display, ctx, cancel, wg)
-		}
-		return cenums.EVENT_STOP
-	}
-}
-
-func WrapArgvNoneWithFlagsSignal(fn func() cenums.EventFlag) cdk.SignalListenerFn {
-	return func(_ []interface{}, _ ...interface{}) cenums.EventFlag {
-		return fn()
-	}
-}
-
-func WrapArgvNoneSignal(fn func(), eventFlag cenums.EventFlag) cdk.SignalListenerFn {
-	return func(_ []interface{}, _ ...interface{}) cenums.EventFlag {
-		fn()
-		return eventFlag
-	}
-}
-
+// NewApplicationFromPlugin constructs an Application from a Go plugin shared
+// object file. This file must export a number of variables and functions, as
+// follows:
+//
+//  Variables
+//  ---------
+//   CtkName         the name of the program binary
+//   CtkUsage        brief summary of what the program is used for
+//   CtkDescription  long description of the program
+//   CtkVersion      the version number to report
+//   CtkTag          a machine tag name, ie: "tld.domain.name" ([-.a-zA-Z])
+//   CtkTitle        the user-visible name of the program
+//   CtkTtyPath      the unix tty device path for display capture
+//
+//  Functions
+//  ---------
+//   // early initialization stage
+//   Init(Application)
+//   // UI initialization stage
+//   Startup(Application, cdk.Display, context.Context, context.CancelFunc, *sync.WaitGroup) cenums.EventFlag
+//   // Application shutdown stage (display destroyed, logging still works, stdin/stdout restored)
+//   Shutdown() cenums.EventFlag
+//
+// Two things must happen in the Startup function for CTK to correctly start
+// rendering the user-interface. The first is to ensure that the function
+// returns EVENT_PASS for the SignalStartup to complete. Returning EVENT_STOP
+// will abort the entire startup process and immediately result in system
+// shutdown. The second thing that must happen for startup to complete is that
+// Application.NotifyStartupComplete must be called before returning EVENT_PASS.
+// This will notify the Application and enable the Display to begin rending to
+// the Screen instance.
 func NewApplicationFromPlugin(path string) (app Application, err error) {
 	var plug *plugin.Plugin
 	if plug, err = plugin.Open(path); err != nil {
 		return nil, err
 	}
-	var initFn ApplicationPluginInitFn
+	var initFn ApplicationInitFn
 	if initFn, err = lookupPluginInitFn(plug); err != nil {
 		return nil, err
 	}
-	var startupFn ApplicationPluginStartupFn
+	var startupFn ApplicationStartupFn
 	if startupFn, err = lookupPluginStartupFn(plug); err != nil {
 		return nil, err
 	}
-	var shutdownFn ApplicationPluginShutdownFn
+	var shutdownFn ApplicationShutdownFn
 	if shutdownFn, err = lookupPluginShutdownFn(plug); err != nil {
 		return nil, err
 	}
@@ -114,6 +102,49 @@ func NewApplicationFromPlugin(path string) (app Application, err error) {
 	return
 }
 
+func ArgvApplicationSignalStartup(argv ...interface{}) (app Application, display cdk.Display, ctx context.Context, cancel context.CancelFunc, wg *sync.WaitGroup, ok bool) {
+	if len(argv) == 5 {
+		if app, ok = argv[0].(Application); ok {
+			if display, ok = argv[1].(cdk.Display); ok {
+				if ctx, ok = argv[2].(context.Context); ok {
+					if cancel, ok = argv[3].(context.CancelFunc); ok {
+						if wg, ok = argv[4].(*sync.WaitGroup); ok {
+							return
+						}
+						cancel = nil
+					}
+					ctx = nil
+				}
+				display = nil
+			}
+			app = nil
+		}
+	}
+	return
+}
+
+func WithArgvApplicationSignalStartup(startupFn ApplicationStartupFn) cdk.SignalListenerFn {
+	return func(_ []interface{}, argv ...interface{}) cenums.EventFlag {
+		if app, display, ctx, cancel, wg, ok := ArgvApplicationSignalStartup(argv...); ok {
+			return startupFn(app, display, ctx, cancel, wg)
+		}
+		return cenums.EVENT_STOP
+	}
+}
+
+func WithArgvNoneWithFlagsSignal(fn func() cenums.EventFlag) cdk.SignalListenerFn {
+	return func(_ []interface{}, _ ...interface{}) cenums.EventFlag {
+		return fn()
+	}
+}
+
+func WithArgvNoneSignal(fn func(), eventFlag cenums.EventFlag) cdk.SignalListenerFn {
+	return func(_ []interface{}, _ ...interface{}) cenums.EventFlag {
+		fn()
+		return eventFlag
+	}
+}
+
 func lookupPluginStringValue(key string, plug *plugin.Plugin) (value string, err error) {
 	var symbol plugin.Symbol
 	if symbol, err = plug.Lookup(key); err != nil {
@@ -128,14 +159,14 @@ func lookupPluginStringValue(key string, plug *plugin.Plugin) (value string, err
 	return
 }
 
-func lookupPluginInitFn(plug *plugin.Plugin) (initFn ApplicationPluginInitFn, err error) {
+func lookupPluginInitFn(plug *plugin.Plugin) (initFn ApplicationInitFn, err error) {
 	var symbol plugin.Symbol
 	if symbol, err = plug.Lookup(ApplicationPluginInitFnName); err != nil {
 		return nil, err
 	}
-	if symPtr, ok := symbol.(ApplicationPluginInitFn); !ok {
+	if symPtr, ok := symbol.(ApplicationInitFn); !ok {
 		return nil, fmt.Errorf(
-			"%v func stored in plugin is not of ApplicationPluginInitFn type: %v (%T)\n",
+			"%v func stored in plugin is not of ApplicationInitFn type: %v (%T)\n",
 			ApplicationPluginInitFnName,
 			symbol,
 			symbol,
@@ -146,14 +177,14 @@ func lookupPluginInitFn(plug *plugin.Plugin) (initFn ApplicationPluginInitFn, er
 	return
 }
 
-func lookupPluginStartupFn(plug *plugin.Plugin) (startupFn ApplicationPluginStartupFn, err error) {
+func lookupPluginStartupFn(plug *plugin.Plugin) (startupFn ApplicationStartupFn, err error) {
 	var symbol plugin.Symbol
 	if symbol, err = plug.Lookup(ApplicationPluginStartupFnName); err != nil {
 		return nil, err
 	}
-	if symPtr, ok := symbol.(ApplicationPluginStartupFn); !ok {
+	if symPtr, ok := symbol.(ApplicationStartupFn); !ok {
 		return nil, fmt.Errorf(
-			"%v func stored in plugin is not of ApplicationPluginStartupFn type: %v (%T)\n",
+			"%v func stored in plugin is not of ApplicationStartupFn type: %v (%T)\n",
 			ApplicationPluginStartupFnName,
 			symbol,
 			symbol,
@@ -164,14 +195,14 @@ func lookupPluginStartupFn(plug *plugin.Plugin) (startupFn ApplicationPluginStar
 	return
 }
 
-func lookupPluginShutdownFn(plug *plugin.Plugin) (shutdownFn ApplicationPluginShutdownFn, err error) {
+func lookupPluginShutdownFn(plug *plugin.Plugin) (shutdownFn ApplicationShutdownFn, err error) {
 	var symbol plugin.Symbol
 	if symbol, err = plug.Lookup(ApplicationPluginShutdownFnName); err != nil {
 		return nil, err
 	}
-	if symPtr, ok := symbol.(ApplicationPluginShutdownFn); !ok {
+	if symPtr, ok := symbol.(ApplicationShutdownFn); !ok {
 		return nil, fmt.Errorf(
-			"%v func stored in plugin is not of ApplicationPluginShutdownFn type: %v (%T)\n",
+			"%v func stored in plugin is not of ApplicationShutdownFn type: %v (%T)\n",
 			ApplicationPluginShutdownFnName,
 			symbol,
 			symbol,
@@ -183,12 +214,15 @@ func lookupPluginShutdownFn(plug *plugin.Plugin) (shutdownFn ApplicationPluginSh
 }
 
 var ApplicationPluginInitFnName = "CtkInit"
-
-type ApplicationPluginInitFn = func(app Application)
-
 var ApplicationPluginStartupFnName = "CtkStartup"
+var ApplicationPluginShutdownFnName = "CtkShutdown"
 
-type ApplicationPluginStartupFn = func(
+const ApplicationPluginStartupHandle = "application-plugin-startup-handler"
+const ApplicationPluginShutdownHandle = "application-plugin-shutdown-handler"
+
+type ApplicationInitFn = func(app Application)
+
+type ApplicationStartupFn = func(
 	app Application,
 	display cdk.Display,
 	ctx context.Context,
@@ -196,10 +230,4 @@ type ApplicationPluginStartupFn = func(
 	wg *sync.WaitGroup,
 ) cenums.EventFlag
 
-var ApplicationPluginShutdownFnName = "CtkShutdown"
-
-type ApplicationPluginShutdownFn = func() cenums.EventFlag
-
-const ApplicationPluginStartupHandle = "application-plugin-startup-handler"
-
-const ApplicationPluginShutdownHandle = "application-plugin-shutdown-handler"
+type ApplicationShutdownFn = func() cenums.EventFlag
