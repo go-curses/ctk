b1b66ea63b8def5e3f89308fc669a2437c7797ed
Kevin C. Krinke <kevin@krinke.ca>
2022-07-03 15:13:55 -0400

    widget (helpers): refactored for a generic WidgetDescend, used by WidgetRecurse{SetWindow,Invalidate}

--- widget_helpers.go
+++ widget_helpers.go
@@ -1,29 +1,45 @@
 package ctk
 
-func WidgetRecurseSetWindow(child Widget, window Window) {
-	if child != nil {
-		child.SetWindow(window)
-		for _, composite := range child.GetCompositeChildren() {
-			composite.SetWindow(window)
+import (
+	cenums "github.com/go-curses/cdk/lib/enums"
+)
+
+type WidgetIteratorFn = func(target Widget) cenums.EventFlag
+
+func WidgetDescend(widget Widget, fn WidgetIteratorFn) (rv cenums.EventFlag) {
+	if widget != nil {
+		for _, composite := range widget.GetCompositeChildren() {
+			if rv = fn(composite); rv == cenums.EVENT_STOP {
+				return
+			}
 		}
-		if container, ok := child.Self().(Container); ok {
+		if container, ok := widget.Self().(Container); ok {
 			for _, grandchild := range container.GetChildren() {
-				WidgetRecurseSetWindow(grandchild, window)
+				if rv = WidgetDescend(grandchild, fn); rv == cenums.EVENT_STOP {
+					return
+				}
 			}
 		}
 	}
+	return
 }
 
-func WidgetRecurseInvalidate(child Widget) {
-	if child != nil {
-		child.SetInvalidated(true)
-		for _, composite := range child.GetCompositeChildren() {
-			composite.SetInvalidated(true)
-		}
-		if container, ok := child.Self().(Container); ok {
-			for _, grandchild := range container.GetChildren() {
-				WidgetRecurseInvalidate(grandchild)
-			}
-		}
+func WidgetRecurseSetWindow(widget Widget, window Window) {
+	if widget != nil {
+		widget.SetWindow(window)
+		WidgetDescend(widget, func(target Widget) cenums.EventFlag {
+			target.SetWindow(window)
+			return cenums.EVENT_PASS
+		})
+	}
+}
+
+func WidgetRecurseInvalidate(widget Widget) {
+	if widget != nil {
+		widget.SetInvalidated(true)
+		WidgetDescend(widget, func(target Widget) cenums.EventFlag {
+			target.SetInvalidated(true)
+			return cenums.EVENT_PASS
+		})
 	}
 }
